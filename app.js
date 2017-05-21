17c97a039f51453c7bcc22097dd0940b3dcf4e8b
Carla Hy <carla.hyenne@gmail.com>
2017-05-21 17:59:26 +0100

    Build ng app

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 const users = require('./routes/users');
 
 // Port number
-const port = 3000;
+const port = process.env.port || 8080;
 
 // CORS middleware
 app.use(cors());
@@ -45,6 +45,10 @@ app.use('/users', users);
 // Index route
 app.get('/', (req, res) => {
   res.send('Invalid Endpoint');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start server
