2f2618b6d9f5ae049ad2a6db638a4ffc5daff080
Carla Hy <carla.hyenne@gmail.com>
2017-06-01 17:23:34 +0100

    Switching db to heroku prod

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // database: 'mongodb://carlahy:carlahy@ds147821.mlab.com:47821/osteapp', //prod
-  database: 'mongodb://localhost:27017/osteoapp',    //dev
+  database: 'mongodb://carlahy:carlahy@ds147821.mlab.com:47821/osteapp', //prod
+  // database: 'mongodb://localhost:27017/osteoapp',    //dev
   secret: 'mysecret'
 }
